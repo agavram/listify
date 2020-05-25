ad3132df24b94794cadc189afdcb6e0a624b130e
Adrian Avram <Adrian_Avram@Outlook.com>
2020-05-25 09:37:39 -0700

    Initial commit

---
+++ src/pages/index.svelte
@@ -0,0 +1,10 @@
+<script>
+</script>
+
+<div>
+<h1>
+Hello
+<button type="button" class="btn btn-primary">Primary</button>
+
+</h1>
+</div>
---
+++ src/pages/lists/[index].svelte
@@ -0,0 +1,22 @@
+<script>
+import { params } from '@sveltech/routify'
+console.log($params.index)
+</script>
+
+<div style="width: 100%; text-align: center; margin-top: 4rem;">
+  <div>
+    <b>Guide:</b>
+    <br />
+    <a href="https://routify.dev">https://routify.dev</a>
+  </div>
+
+  <br />
+  <div>
+    <b>This template:</b>
+    <br />
+    <a href="https://github.com/sveltech/routify-starter">
+      https://github.com/sveltech/routify-starter
+    </a>
+  </div>
+
+</div>
