8fa3a66224a797eaf81bce7897e1030b1ede87bf
Adrian Avram <Adrian_Avram@Outlook.com>
2020-05-25 19:34:04 -0700

    Added swapping

---
+++ src/_components/List.svelte
@@ -0,0 +1,20 @@
+<script>
+  import { onMount } from "svelte";
+  import ListItem from "./ListItem.svelte";
+  import { createEventDispatcher } from "svelte";
+
+  const dispatch = createEventDispatcher();
+
+  onMount(async () => {
+    dispatch("listCreated", {});
+  });
+
+  export let list;
+  export let index;
+</script>
+
+<ul class="list-group" id="items">
+  {#each list as item}
+    <ListItem {...item} {index} />
+  {/each}
+</ul>
--- src/pages/lists/[index].svelte
+++ src/pages/lists/[index].svelte
@@ -1,34 +1,102 @@
 <script>
   import { collectionData } from "rxfire/firestore";
   import { db } from "../../firebase";
-  import ListItem from "../../_components/ListItem.svelte"
+  import ListItem from "../../_components/ListItem.svelte";
+  import SortableList from "svelte-sortable-list";
 
   export let index;
 
-  const ref = db.collection("lists").doc(index).collection("list").orderBy('generated');
+  const ref = db
+    .collection("lists")
+    .doc(index)
+    .collection("list")
+    .orderBy("generated");
   let list;
   let input = "";
 
-  ref.limit(1).get().then(query => {
-    if (query.size >= 1) {
-      collectionData(ref, 'id').subscribe(res => {
-        res.shift();
-        list = res;
-      })
-    } else {
-      list = "error";
-    }
-  });
+  ref
+    .limit(1)
+    .get()
+    .then(query => {
+      if (query.size >= 1) {
+        collectionData(ref, "id").subscribe(res => {
+          res.shift();
+          list = res;
+        });
+      } else {
+        list = "error";
+      }
+    });
 
   async function append(e) {
     e.preventDefault();
-    db.collection('lists').doc(index).collection("list").doc().set({
-      label: input,
-      complete: false,
-      generated: Date.now()
-    }).then(() => {
-      input = "";
-    })
+    db.collection("lists")
+      .doc(index)
+      .collection("list")
+      .doc()
+      .set({
+        label: input,
+        complete: false,
+        generated: Date.now()
+      })
+      .then(() => {
+        input = "";
+      });
+  }
+
+  function sortList(e) {
+    let swap = [];
+    for (let i = 0; i < list.length; i++) {
+      if (e.detail[i].id != list[i].id) {
+        swap.push(i);
+      }
+    }
+    if (swap.length == 2) {
+      swapItems(swap[0], swap[1]);
+    }
+  }
+
+  async function swapItems(newIndex, oldIndex) {
+    let newGenerate = await db
+      .collection("lists")
+      .doc(index)
+      .collection("list")
+      .doc(list[newIndex].id)
+      .get();
+    newGenerate = newGenerate.data().generated;
+
+    let oldGenerate = await db
+      .collection("lists")
+      .doc(index)
+      .collection("list")
+      .doc(list[oldIndex].id)
+      .get();
+    oldGenerate = oldGenerate.data().generated;
+
+    let batch = db.batch();
+    batch.update(
+      db
+        .collection("lists")
+        .doc(index)
+        .collection("list")
+        .doc(list[newIndex].id),
+      {
+        generated: oldGenerate
+      }
+    );
+
+    batch.update(
+      db
+        .collection("lists")
+        .doc(index)
+        .collection("list")
+        .doc(list[oldIndex].id),
+      {
+        generated: newGenerate
+      }
+    );
+
+    batch.commit();
   }
 </script>
 
@@ -40,9 +108,9 @@
   <div class="d-flex justify-content-around mt-3">
     <div class="col-6">
       <ul class="list-group">
-        {#each list as item}
+        <SortableList {list} key="id" on:sort={sortList} let:item>
           <ListItem {...item} {index} />
-        {/each}
+        </SortableList>
       </ul>
     </div>
   </div>
@@ -56,7 +124,9 @@
           aria-describedby="inputGroup-sizing-default"
           bind:value={input}
           placeholder="Enter something" />
-        <button type="button" class="btn btn-primary ml-3" on:click={append}>Add</button>
+        <button type="button" class="btn btn-primary ml-3" on:click={append}>
+          Add
+        </button>
       </div>
     </div>
 
